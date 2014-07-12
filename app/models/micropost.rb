9e7692212b85c1d9a1d2415bd55943b1d097b884
zzx128zzx128 <372400016@qq.com>
2014-07-12 22:32:00 +0800

    Update micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,8 @@
 class Micropost < ActiveRecord::Base
   validates :content, length: { maximum: 140 }
 end
+
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  validates :content, length: { maximum: 140 }
+end
